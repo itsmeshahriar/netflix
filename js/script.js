5e0a63ce7cdba13b34b0aef8d4f0b42dd0d15841
itsmeshahriar <62495742+itsmeshahriar@users.noreply.github.com>
2020-04-27 23:07:01 +0600

    uploaded all site files

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+//Select tab content item
+function selectItem(event) {
+	removeBorder();
+	removeShow();
+	// add border to current tab
+	this.classList.add("tab-border");
+	//grab the clicked tab
+	console.log(this.id)
+	//show the clicked tab
+	const tabContentItem = document.querySelector(`#${this.id}-content`); 
+	tabContentItem.classList.add("show")
+}
+
+function removeBorder(){
+	tabItems.forEach(item => item.classList.remove("tab-border"))
+}
+
+function removeShow(){
+	tabContentItems.forEach(item => item.classList.remove("show"))
+}
+
+
+//Listen for tab click
+tabItems.forEach(item => item.addEventListener("click", selectItem) );
